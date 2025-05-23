a5cc424245142ab7dba080767895bac0895f4903
SHAFIDEVEL0PER <Shafikov.p@mail.ru>
2025-05-23 12:18:11 +0300

    Тест: автоматическая генерация документации

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * @file Основной скрипт для веб-приложения.
+ * @author Ваше Имя
+ * @version 1.0.0
+ */
+
+/**
+ * Вычисляет сумму двух чисел.
+ * @param {number} a - Первое число.
+ * @param {number} b - Второе число.
+ * @returns {number} Сумма a и b.
+ */
+function sum(a, b) {
+    return a + b;
+}
